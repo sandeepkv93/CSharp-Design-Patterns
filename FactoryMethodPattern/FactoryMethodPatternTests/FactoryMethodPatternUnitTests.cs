e1a9ab31278007759fe5d359560572218ab6f6b5
Sandeep Vishnu <sandeepkv93@gmail.com>
2020-11-25 18:12:25 -0800

    Added negative tests to FactoryMethodPattern

--- FactoryMethodPattern/FactoryMethodPatternTests/FactoryMethodPatternUnitTests.cs
+++ FactoryMethodPattern/FactoryMethodPatternTests/FactoryMethodPatternUnitTests.cs
@@ -1,4 +1,5 @@
 using FactoryMethodPattern;
+using System;
 using Xunit;
 
 namespace FactoryMethodPatternTests
@@ -8,9 +9,26 @@ namespace FactoryMethodPatternTests
         [Fact]
         public void GetSavingsAccount_CitiSavingsAcctFromFactory_Success()
         {
-            ICreditUnionFactory factory = new SavingsAcctFactory();
+            ICreditUnionFactory factory = new SavingsAcctFactory() as ICreditUnionFactory;
             ISavingsAccount citiAcct = factory.GetSavingsAccount("CITI-321");
             Assert.True(citiAcct is CitiSavingsAcct);
+        }
+
+        [Fact]
+        public void GetSavingsAccount_NationalSavingsAcctFromFactory_Success()
+        {
+            ICreditUnionFactory factory = new SavingsAcctFactory() as ICreditUnionFactory;
+            ISavingsAccount nationalAcct = factory.GetSavingsAccount("NATIONAL-987");
+            Assert.True(nationalAcct is NationalSavingsAcct);
+        }
+
+        [Fact]
+        public void GetSavingsAccount_ChaseSavingsAcctFromFactory_ThrowsArgumentException()
+        {
+            ICreditUnionFactory factory = new SavingsAcctFactory() as ICreditUnionFactory;
+            ArgumentException exception = Assert.Throws<ArgumentException>(
+                () => factory.GetSavingsAccount("CHASE-845"));
+            Assert.Equal("Invalid Account Number", exception.Message);
         }
     }
 }
